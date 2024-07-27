681d3ee9a370f1f19b121b30c6fc81e53540329c
Divyarajsinh Zala <divyarajsinh5171@gmail.com>
2024-07-27 13:46:46 -0400

    Initial commit

---
+++ FishApp.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify, render_template
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+# Load model
+model = joblib.load('fish_model.pkl')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    prediction = model.predict(np.array(data['features']).reshape(1, -1))
+    return jsonify({'prediction': prediction[0]})
+
+if __name__ == '__main__':
+    app.run(debug=True)
